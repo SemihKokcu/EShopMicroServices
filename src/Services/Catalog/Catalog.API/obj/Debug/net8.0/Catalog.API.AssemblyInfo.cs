b01d454856431a777be1a8387cf3a3f4f0a8ca48
SemihKokcu <semihkkc1@gmail.com>
2025-10-14 21:32:12 +0300

    ordering service recreate with correct way

--- src/Services/Catalog/Catalog.API/obj/Debug/net8.0/Catalog.API.AssemblyInfo.cs
+++ src/Services/Catalog/Catalog.API/obj/Debug/net8.0/Catalog.API.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Catalog.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb1c6f112cc0fec6ec631e093c4581cde84a115e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0869e8323e4ad430634e3d7d2ecc3c2bbfb60f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Catalog.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Catalog.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
